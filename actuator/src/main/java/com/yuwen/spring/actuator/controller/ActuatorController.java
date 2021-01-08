fa79d1b7e2334c16799881e0c11050474e35a587
yuwen <yuwen3@asiainfo.com>
2021-01-08 14:23:56 +0800

    创建SpringBoot集成Elasticsearch工程

--- actuator/src/main/java/com/yuwen/spring/actuator/controller/ActuatorController.java
+++ actuator/src/main/java/com/yuwen/spring/actuator/controller/ActuatorController.java
@@ -1,13 +1,18 @@
 package com.yuwen.spring.actuator.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActuatorController {
 
+	@Value("info.description")
+	private String name;
+
 	@GetMapping("hello")
 	public String helloActutor() {
+		System.out.println("name=" + name);
 		return "hello actutor";
 	}
 }
