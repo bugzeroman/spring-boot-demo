945f6309630968bed92250cc3c2c117a69ec232b
yuwen <yuwen3@asiainfo.com>
2021-04-14 14:49:23 +0800

    Spring Boot集成ehcache,但是不使用注解，使用自定义高级代码

---
+++ cache/ehcache-advance/src/main/java/com/yuwen/spring/demo/controller/impl/UserControllerImpl.java
@@ -0,0 +1,73 @@
+package com.yuwen.spring.demo.controller.impl;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yuwen.spring.demo.controller.UserController;
+import com.yuwen.spring.demo.entity.User;
+
+/**
+ * 使用CacheManager自己管理缓存，不使用基于注解的方式
+ *
+ */
+@RestController
+public class UserControllerImpl implements UserController {
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	@Override
+	public void createUser(User user) {
+		System.out.println("createUser, user=" + user);
+
+	}
+
+	@Override
+	public User upadteUser(Long id, User user) {
+		System.out.println("upadteUser, id=" + id + ", user=" + user);
+		return user;
+	}
+
+	@Override
+	public void deleteUser(Long id) {
+		System.out.println("deleteUser, id=" + id);
+	}
+
+	@Override
+	public User getOneUser(Long id) {
+		// 先从缓存中读取
+		Cache cache = cacheManager.getCache(USER_CACHE_NAME);
+		String key = "key_" + id;
+		User userCache = cache.get(key, User.class);
+		if (userCache != null) {
+			System.out.println("getOneUser, find cache user=" + userCache);
+			return userCache;
+		}
+
+		System.out.println("getOneUser, id=" + id);
+		User user = new User();
+		user.setId(id);
+
+		// 把找到的放进缓存
+		cache.put(key, user);
+
+		return user;
+	}
+
+	@Override
+	public List<User> getAllUser(Integer pageNum, Integer pageSize) {
+		System.out.println("getAllUser, pageNum=" + pageNum + ", pageSize=" + pageSize);
+		User user = new User();
+		user.setId(999L);
+		user.setName("all");
+		user.setBirthday(new Date());
+		return Collections.singletonList(user);
+	}
+
+}
