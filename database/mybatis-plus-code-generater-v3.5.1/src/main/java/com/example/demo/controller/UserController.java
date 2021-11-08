026b1a0f056c07e4ce1fd05a59147a806883744f
yuwen <yuwen3@asiainfo.com>
2021-11-08 18:03:22 +0800

    v3.5.1新方式自动生成的代码

---
+++ database/mybatis-plus-code-generater-v3.5.1/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author yuwen
+ * @since 2021-11-08
+ */
+@Controller
+@RequestMapping("/demo/user")
+public class UserController {
+
+}
