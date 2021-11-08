89d23d8129c6102ddd093bfa4b76a5211f77eb46
yuwen <yuwen3@asiainfo.com>
2021-11-08 18:02:45 +0800

    生成代码的SQL脚本，需要更新

---
+++ database/db/MySQL/init_data_role.sql
@@ -0,0 +1,6 @@
+DELETE FROM TBL_ROLE;
+
+INSERT INTO TBL_ROLE (id, role_name, role_describe)
+VALUES (1, 'admin', 'boos level'),
+       (2, 'user', 'just a normal people'),
+       (3, 'programer', 'sometime like a god');
