1926e98456c66766132a2bdcd33244585876550a
yuwen <yuwen3@asiainfo.com>
2021-11-09 10:31:44 +0800

    细化代码生成器配置

--- database/mybatis-plus-code-generater-v3.5.1/src/main/java/com/yuwen/spring/uitl/generate/CodeGenerator.java
+++ database/mybatis-plus-code-generater-v3.5.1/src/main/java/com/yuwen/spring/uitl/generate/CodeGenerator.java
@@ -3,8 +3,14 @@ package com.yuwen.spring.uitl.generate;
 import java.util.Collections;
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.querys.MySqlQuery;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.engine.AbstractTemplateEngine;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.keywords.MySqlKeyWordsHandler;
 
 /**
  * 
@@ -13,7 +19,7 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 public class CodeGenerator {
 
 	// 数据库连接配置
-	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	// private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String JDBC_URL = "jdbc:mysql://10.21.13.14:3306/demodb?useUnicode=true&useSSL=false&characterEncoding=utf8";
 	private static final String JDBC_USER_NAME = "demo";
 	private static final String JDBC_PASSOWRD = "demo123456";
@@ -30,21 +36,42 @@ public class CodeGenerator {
 
 	// 生成代码入口main方法
 	public static void main(String[] args) {
-		FastAutoGenerator.create(JDBC_URL, JDBC_USER_NAME, JDBC_PASSOWRD).globalConfig(builder -> {
-			builder.author("yuwen") // 设置作者
-					// .enableSwagger() // 开启 swagger 模式
-					.fileOverride() // 覆盖已生成文件
-					.disableOpenDir()// 不打开生成文件目录
-					.outputDir("src\\main\\java"); // 指定输出目录,注意使用反斜杠\\
-		}).packageConfig(builder -> {
-			builder.parent(PACKAGE_NAME) // 设置父包名
-					.moduleName(MODULE_NAME) // 设置父包模块名
-					.pathInfo(Collections.singletonMap(OutputFile.mapperXml, "src\\main\\resources")); // 设置mapperXml生成路径
-		}).strategyConfig(builder -> {
-			builder.addInclude(TBL_NAMES) // 设置需要生成的表名
-					.addTablePrefix(TABLE_PREFIX); // 设置过滤表前缀
-		}).templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
-				.execute();
+		// 1.数据库配置
+		DataSourceConfig.Builder dataSourceConfigBuilder = new DataSourceConfig.Builder(JDBC_URL, JDBC_USER_NAME,
+				JDBC_PASSOWRD).dbQuery(new MySqlQuery()).schema("demodb").typeConvert(new MySqlTypeConvert())
+						.keyWordsHandler(new MySqlKeyWordsHandler());
+
+		// 1.1.快速生成器
+		FastAutoGenerator fastAutoGenerator = FastAutoGenerator.create(dataSourceConfigBuilder);
+
+		// 2.全局配置
+		fastAutoGenerator.globalConfig(globalConfigBuilder -> globalConfigBuilder.fileOverride().disableOpenDir()
+				.outputDir("src\\main\\java").author("yuwen").dateType(DateType.TIME_PACK).commentDate("yyyy-MM-dd"));
+
+		// 3.包配置
+		fastAutoGenerator.packageConfig(packageConfigBuilder -> packageConfigBuilder.parent(PACKAGE_NAME)
+				.moduleName(MODULE_NAME).entity("entity").mapper("dao").xml("mapper.xml").service("service")
+				.serviceImpl("service.impl").controller("controller").other("other")
+				.pathInfo(Collections.singletonMap(OutputFile.mapperXml, "src\\main\\resources")));
+
+		// 4.模板配置
+		AbstractTemplateEngine templateEngine = new FreemarkerTemplateEngine();
+		fastAutoGenerator.templateEngine(templateEngine);
+
+		// 5.注入配置 TODO
+
+		// 6.策略配置
+		fastAutoGenerator.strategyConfig(strategyConfigBuilder -> strategyConfigBuilder.enableCapitalMode()
+				.enableSkipView().disableSqlFilter().addInclude(TBL_NAMES).addTablePrefix(TABLE_PREFIX));
+
+		// 6.1.Entity策略配置 TODO
+
+		// 6.2.Controller策略配置
+		// 6.3.Service策略配置
+		// 6.4.Mapper策略配置
+
+		// 7.生成代码
+		fastAutoGenerator.execute();
 	}
 
 }
