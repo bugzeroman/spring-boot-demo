9d59fb1f8db3d2eba5e03a06b39dba315d27af33
yuwen <yuwen3@asiainfo.com>
2021-11-18 17:41:13 +0800

    优化生成JSON代码

--- scaffold/sd-web-cmn-l4j2-mbp-msl-j5/src/test/java/com/yuwen/spring/jackson/generator/JsonGenerator.java
+++ scaffold/sd-web-cmn-l4j2-mbp-msl-j5/src/test/java/com/yuwen/spring/jackson/generator/JsonGenerator.java
@@ -2,15 +2,18 @@ package com.yuwen.spring.jackson.generator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yuwen.spring.demo.pojo.request.CreateUserReq;
+import com.yuwen.spring.demo.entity.UserEntity;
 
 /**
  * 使用Jackson,指定Java的请求类，生成对应的JSON字符串
  *
  */
 public class JsonGenerator {
+	// 指定需要生成JSON的Java类
+	public static Class<?> targetClass = UserEntity.class;
+
 	public static void main(String[] args) throws Exception {
-		CreateUserReq obj = new CreateUserReq();
+		Object obj = targetClass.newInstance();
 		genetateJson(obj);
 	}
 
